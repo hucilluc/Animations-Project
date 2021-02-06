f41f6420e99064a9c67f1551eec9c9b98246df85
hucilluc <katehenderson@gmx.co.uk>
2021-02-06 11:19:02 +0000

    Tester spehere uploaded succsessfully

---
+++ Animations Project/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/Kate/Desktop/Github/Animations-Project/Animations Project/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1666672591537963781.json
